84e47eba985be036bea9c8e411675270f254c706
Peter Hamilton <peterghamilton@gmail.com>
2015-10-23 22:09:18 -0700

    Update mix config for release

--- mix.exs
+++ mix.exs
@@ -35,7 +35,8 @@ defmodule PersonalTwitterBot.Mixfile do
      {:rethinkdb, github: "hamiltop/rethinkdb-elixir"},
      {:phoenix_html, "~> 2.1"},
      {:phoenix_live_reload, "~> 1.0", only: :dev},
-     {:cowboy, "~> 1.0"}]
+     {:cowboy, "~> 1.0"},
+     {:exrm, "~> 0.15.3"}]
   end
 
   # Aliases are shortcut or tasks specific to the current project.
