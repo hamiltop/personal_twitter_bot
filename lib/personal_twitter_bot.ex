7880698cab2db647b38679fcd3054797d721b15d
Peter Hamilton <peterghamilton@gmail.com>
2015-10-25 22:05:19 -0700

    move twitter config to runtime

--- lib/personal_twitter_bot.ex
+++ lib/personal_twitter_bot.ex
@@ -6,6 +6,17 @@ defmodule PersonalTwitterBot do
   def start(_type, _args) do
     import Supervisor.Spec, warn: false
 
+    case Application.get_env(:extwitter, :oauth) do
+      nil ->
+        ExTwitter.configure(
+          consumer_key: System.get_env("TWITTER_CONSUMER_KEY"),
+          consumer_secret: System.get_env("TWITTER_CONSUMER_SECRET"),
+          access_token: System.get_env("TWITTER_ACCESS_TOKEN"),
+          access_token_secret: System.get_env("TWITTER_ACCESS_SECRET")
+        )
+      _ -> :ok
+    end
+ 
     children = [
       # Start the endpoint when the application starts
       supervisor(PersonalTwitterBot.Endpoint, []),
