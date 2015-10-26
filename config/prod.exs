31e8317503c55079ee59f8335e278c92356c9425
Peter Hamilton <peterghamilton@gmail.com>
2015-10-25 22:17:06 -0700

    remove twitter conf from prod config

--- config/prod.exs
+++ config/prod.exs
@@ -21,13 +21,6 @@ config :personal_twitter_bot, PersonalTwitterBot.Endpoint,
 # Do not print debug messages in production
 config :logger, level: :info
 
-config :extwitter, :oauth, [
-   consumer_key: System.get_env("TWITTER_CONSUMER_KEY"),
-   consumer_secret: System.get_env("TWITTER_CONSUMER_SECRET"),
-   access_token: System.get_env("TWITTER_ACCESS_TOKEN"),
-   access_token_secret: System.get_env("TWITTER_ACCESS_SECRET")
-]
-
 # ## SSL Support
 #
 # To get SSL working, you will need to add the `https` key
